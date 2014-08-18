f5662e9a850a8b709a5c70c9b1b8d64e62044a75
BusinessCereal <htfu513@gmail.com>
2014-08-17 21:21:05 -0500

    complex arithmetic

---
+++ complex.py
@@ -0,0 +1,31 @@
+def add_complex(a,b):
+	return [(a[0] + b[0]), (a[1] + b[1])]
+
+
+
+
+
+#x and y are complex numbers (2 element arrays)
+def mult_complex(a,b):
+	#waht
+	pass
+
+def sub_complex(a,b):
+	return [(a[0] - b[0]), (a[1] - b[1])]
+
+
+
+
+
+if (add_complex([1,1], [2,2]) != [3,3]):
+	print "add_complex pooped itself"
+
+if (add_complex([1,4], [2,5]) != [3,9]):
+	print "add_complex pooped itself"
+
+if (sub_complex([0,82], [42,42]) != [-42, 40]):
+	print "sub_complex pooped itself"
+
+if (sub_complex([9,9], [0,13]) != [9, -4]):
+	print "sub_complex pooped itself"
+
