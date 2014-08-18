b66b84924d85a9311d32558ba9f76ff9f3537fb4
BusinessCereal <htfu513@gmail.com>
2014-08-17 21:36:24 -0500

    added multiplication

--- complex.py
+++ complex.py
@@ -7,8 +7,7 @@ def add_complex(a,b):
 
 #x and y are complex numbers (2 element arrays)
 def mult_complex(a,b):
-	#waht
-	pass
+	return [(a[0] * b[0] - a[1] * b[1]), (a[0] * b[1] + b[0] * a[1])]
 
 def sub_complex(a,b):
 	return [(a[0] - b[0]), (a[1] - b[1])]
@@ -29,3 +28,5 @@ if (sub_complex([0,82], [42,42]) != [-42, 40]):
 if (sub_complex([9,9], [0,13]) != [9, -4]):
 	print "sub_complex pooped itself"
 
+if (mult_complex([3,5], [6,7]) != [-17, 51]):
+	print "mult_complex pooped itself"
