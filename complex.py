c6ad0ff542d03cbbdb3194d998a97e7d6febe5e8
BusinessCereal <htfu513@gmail.com>
2014-09-14 20:11:54 -0500

    created the distance formula

--- complex.py
+++ complex.py
@@ -9,24 +9,40 @@ def mult_complex(a,b):
 
 def sub_complex(a,b):
 	return [(a[0] - b[0]), (a[1] - b[1])]
+ 
+def sqr_complex(a):
+    return mult_complex(a,a)
 
-def dev_complex(a,b):
-	pass
+#returns how many iterations until transgretion
+# or negative value if reached max iterations 
 
+
+def mand(C, max_distance, max_iterations):
+    pass
+
+
+# Z = C
+# Z = add_complex(sqr_complex(2), C)
+
+import math
+
+def dist(a,b):
+    return math.sqrt((b[1] - a[1]) * (b[1] - a[1])  + (b[0] - a[0]) * (b[0] - a[0]))
 
 if (add_complex([1,1], [2,2]) != [3,3]):
-	print "add_complex pooped itself"
+	print "add_complex Fail"
 
 if (add_complex([1,4], [2,5]) != [3,9]):
-	print "add_complex pooped itself"
+	print "add_complex Fail"
 
 if (sub_complex([0,82], [42,42]) != [-42, 40]):
-	print "sub_complex pooped itself"
+	print "sub_complex Fail"
 
 if (sub_complex([9,9], [0,13]) != [9, -4]):
-	print "sub_complex pooped itself"
+	print "sub_complex Fail"
 
 if (mult_complex([3,5], [6,7]) != [-17, 51]):
-	print "mult_complex pooped itself"
+	print "Mult_complex Fail"
 
-if (dev_complex([]))
+if (dist([-2,-3], [-4,4]) != math.sqrt(53)):
+	print dist([-2,-3], [-4,4])
